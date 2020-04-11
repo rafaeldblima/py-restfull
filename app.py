9f8d781f85894ef47855d2c07fa92e6fa3017a40
Rafael Lima <rafaeldblima@gmail.com>
2020-04-11 14:57:23 -0400

    Adding details endpoints

--- app.py
+++ app.py
@@ -11,3 +11,13 @@ def home(request, response):
 @app.route("/about")
 def about(request, response):
     response.text = "Hello from the ABOUT page"
+
+
+@app.route("/hello/{name}")
+def say_hello(request, response, name):
+    response.text = f"Hello, {name}"
+
+
+@app.route("/tell/{age:d}")
+def tell(request, response, age):
+    response.text = f"Your age: {age}"
