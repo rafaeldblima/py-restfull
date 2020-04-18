b1a131d4d9a704998188241918f036b513166d3c
Rafael Lima <rafaeldblima@gmail.com>
2020-04-18 16:54:20 -0400

    Adicionando modelo Book

--- database/models.py
+++ database/models.py
@@ -1,15 +1,40 @@
-from ming import schema
-from ming.odm import FieldProperty
-from ming.odm import MappedClass
+from datetime import datetime
+
+from ming.odm import FieldProperty, MappedClass, ThreadLocalODMSession, MapperExtension
+from ming.schema import ObjectId, String, NumberDecimal, DateTime
 
 from .session import session
 
 
-class Book(MappedClass):
+class BaseClass(MappedClass):
     class __mongometa__:
-        session = session
+        session: ThreadLocalODMSession = None
+        name = None
+
+    def save(self):
+        self.__mongometa__.session.flush_all()
+
+
+class UpdatedAtExtension(MapperExtension):
+    def before_update(self, instance, state, sess):
+        print('passou')
+        instance.updated_at = datetime.utcnow()
+
+
+class Timeable(MappedClass):
+    created_at = FieldProperty(DateTime(if_missing=datetime.utcnow))
+    updated_at = FieldProperty(DateTime(if_missing=datetime.utcnow))
+
+    class __mongometa__:
+        extensions = [UpdatedAtExtension]
+
+
+class Book(BaseClass, Timeable):
+    class __mongometa__:
+        session: ThreadLocalODMSession = session
         name = 'book'
 
-    _id = FieldProperty(schema.ObjectId)
-    title = FieldProperty(schema.String(required=True))
-    description = FieldProperty(schema.String(if_missing=''))
+    _id = FieldProperty(ObjectId)
+    title = FieldProperty(String(required=True))
+    description = FieldProperty(String(if_missing=''))
+    price = FieldProperty(NumberDecimal(if_missing=0.0, precision=2))
